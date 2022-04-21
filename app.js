792f0ca975941edd3ee3ed620a46e3979e07abab
bkcp <bkcp83@gmail.com>
2022-04-21 06:30:20 -0700

    Call stack and browser debugger

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 document.body.style.backgroundColor = "red";
+
+console.log("sending request to server");
+setTimeout(() => {
+  console.log("data returned");
+}, 2000);
+console.log("end of file reached");
